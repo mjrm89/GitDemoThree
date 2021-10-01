7af09920934bd7b3e8c6b638ed137bff42049a1a
Martha <Martha@10.0.0.245>
2021-09-30 21:20:41 -0400

    save changes

--- src/DemoThree/DThree.java
+++ src/DemoThree/DThree.java
@@ -1,5 +1,17 @@
 package DemoThree;
 
 public class DThree {
+	
+	public static void main(String[] args) {
+		System.out.println("GitDemoThree...");
+		
+		boolean isSunday = true;
+		if(isSunday) {
+			System.out.println("abc");
+		}else {
+			System.out.println("XYZ");
+		}
+	}
+	}
 
 }
